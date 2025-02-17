96bf5c61271223eb4475c96f406384dc77e700af
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-17 17:15:21 +0100

    add es3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,35 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+const userInput = prompt('Inserisci una lettera singola per cercare un nome [es. A,B,C ...').toUpperCase();
+
+const result = [];
+
+function onlyFirstLetter(nameList) {
+
+    for (let i = 0; i < nameList.length; i++) {
+
+        const firstLetter = nameList[i][0];
+
+        if (userInput === firstLetter) {
+            result.push(nameList[i])
+        }
+    }
+
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+onlyFirstLetter(names);
+
+if (result.length === 0) {
+    console.log('Non ci sono nomi con questa lettera')
+} else { 
+    console.log(result) 
+}
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
