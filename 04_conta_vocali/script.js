f8d1ccd1ba6b1e08e760e9e2532cfbd072ac0dce
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-17 17:38:56 +0100

    add es4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,28 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+let result = 0
+
+function countVocals(letter) {
+
+    for (i = 0; i < letter.length; i++) {
+
+       const singleLetter = letter[i]
+
+       if ( singleLetter === 'a' || singleLetter === 'e' || singleLetter === 'i' || singleLetter === 'o' || singleLetter === 'u'){
+
+       result++
+
+       }
+    }
+
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+countVocals(word);
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
