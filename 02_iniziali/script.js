789956f75874a55aa1c66dded978b1c0f414beb4
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-17 16:48:14 +0100

    add es2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+let firstLetter = [ ];
+
+function onlyFirstLetter(nameList) {
+
+    for (let i = 0; i < nameList.length; i++) {
+
+        firstLetter.push(nameList[i][0]);
+    }
+
+    return firstLetter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+onlyFirstLetter(names)
+console.log(firstLetter)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
